2685c190936790e1306d209b27ee845470bcba85
enenflash <hwee.en.tang@gmail.com>
2025-04-23 15:33:05 +0800

    platformio template code

---
+++ pio-template/include/pins.h
@@ -0,0 +1,15 @@
+#define TL_PWM 0
+#define TL_DIR 0
+#define TR_PWM 0
+#define TR_DIR 0
+#define BL_PWM 0
+#define BL_DIR 0
+#define BR_PWM 0
+#define BR_DIR 0
+
+#define UR_TRIG 0
+#define UR_ECHO 0
+#define UL_TRIG 0
+#define UL_ECHO 0
+#define UB_TRIG 0
+#define UB_ECHO 0
